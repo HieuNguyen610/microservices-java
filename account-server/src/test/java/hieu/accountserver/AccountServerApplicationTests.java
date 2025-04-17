2ef8e58ab2765aec39e3e7d7fc8d7a208c27c5bc
hieunm2 <hieunm2@vnext.vn>
2025-04-17 17:01:17 +0700

    acc server

---
+++ account-server/src/test/java/hieu/accountserver/AccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.accountserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
