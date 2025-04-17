f9579f530bfe23b8a59f0cd5550798ddb18572ce
hieunm2 <hieunm2@vnext.vn>
2025-04-17 16:58:56 +0700

    add config

--- auth-server/src/main/java/hieu/authserver/entity/User.java
+++ auth-server/src/main/java/hieu/authserver/entity/User.java
@@ -5,20 +5,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = false)
 @Entity(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
 
-    private String username;
-    private String password;
-    private String role;
+    String username;
+    String password;
+    String role;
 }
