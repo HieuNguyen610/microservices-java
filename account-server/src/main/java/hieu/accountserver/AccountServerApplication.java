4b6ec3f5dfb7f4ed9249aeaf52dd3806fcc023cc
hieunm2 <hieunm2@vnext.vn>
2025-04-18 13:37:30 +0700

    feign client acc-service

--- account-server/src/main/java/hieu/accountserver/AccountServerApplication.java
+++ account-server/src/main/java/hieu/accountserver/AccountServerApplication.java
@@ -2,8 +2,10 @@ package hieu.accountserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AccountServerApplication {
 
     public static void main(String[] args) {
