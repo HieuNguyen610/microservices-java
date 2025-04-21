9026181a52a918a5087d404356b9b1a31f8bc2bd
hieunm2 <hieunm2@vnext.vn>
2025-04-21 13:46:57 +0700

    add log acc-sv

--- account-server/src/main/java/hieu/accountserver/service/impl/AccountServiceImpl.java
+++ account-server/src/main/java/hieu/accountserver/service/impl/AccountServiceImpl.java
@@ -6,6 +6,7 @@ import hieu.accountserver.entity.Account;
 import hieu.accountserver.repository.AccountRepository;
 import hieu.accountserver.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j(topic = "Account-service")
 public class AccountServiceImpl implements AccountService {
 
     private final ObjectMapper objectMapper;
@@ -35,6 +37,7 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void add(AccountDto accountDto) {
+        log.info("Create account..");
         Account newAccount = Account.builder()
                 .name(accountDto.getName())
                 .username(accountDto.getUsername())
@@ -45,6 +48,7 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void update(AccountDto accountDto) {
+        log.info("Update account");
         Account account = accountRepository.findById(accountDto.getId())
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         account.setName(accountDto.getName());
@@ -66,12 +70,14 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public List<AccountDto> getAll() {
+        log.info("Get all accounts info");
         List<Account> accounts = accountRepository.findAll();
         return convertDtos(accounts);
     }
 
     @Override
     public AccountDto getOne(Long id) {
+        log.info("Get account info by id = " + id);
         return convertDto(accountRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("User not found")));
     }
 }
